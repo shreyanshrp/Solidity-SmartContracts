6e25c5f3e15c1154efba283c17a84e6c0f5ad4b6
shreyanshRP <shreyanshrp21@gmail.com>
2022-02-11 23:49:27 +0530

    Create decentralised_calculator.sol
    A Calculator to do basic operations , made in solidity smart contract.

---
+++ decentralised_calculator.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.4.24;
+
+contract Calc {
+    uint c;
+
+    function add(uint a, uint b) public {
+        c = a + b;
+    }
+
+    function sub(uint a, uint b) public {
+        c = a - b;
+    }
+
+     function mul(uint a, uint b) public {
+        c = a * b;
+    }
+
+     function div(uint a, uint b) public {
+         require(b> 0, "The second parameter should be larger than 0");
+        
+        c = a / b;
+    }
+
+    function getResult() public view returns (uint x) {
+        return c;
+    }
+}
+
